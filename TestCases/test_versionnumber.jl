3b66054e12b85e6045a73090433392898764a675
WooKyoung Noh <wookay.noh@gmail.com>
2015-03-20 17:27:49 +0900

    v

--- TestCases/test_versionnumber.jl
+++ TestCases/test_versionnumber.jl
@@ -4,6 +4,12 @@
 include("../juliacat/UnitTest.jl")
 
 function test_version_number()
+  version = v"0.4.0-dev+3929"
+  assert_equal(0, version.major)
+  assert_equal(4, version.minor)
+  assert_equal(0, version.patch)
+  assert_equal(("dev",), version.prerelease)
+  assert_equal((3929,), version.build)
   assert_equal(VersionNumber("1.2.3"), VersionNumber(1,2,3))
 end
 
