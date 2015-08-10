a0436b56c80f75c9c7122519d83c817a78c9b182
WooKyoung Noh <wookay.noh@gmail.com>
2015-08-10 16:35:20 +0200

    LOAD_PATH

--- TestCases/test_loading.jl
+++ TestCases/test_loading.jl
@@ -1,13 +1,13 @@
 # test_loading.jl
 #                           wookay.noh at gmail.com
 
-
-push!(Base.LOAD_CACHE_PATH, dirname(@__FILE__))
-
 include("../juliacat/UnitTest.jl")
 
 
 if VERSION.minor > 3 @eval begin
+
+push!(Base.LOAD_PATH, dirname(@__FILE__))
+push!(Base.LOAD_CACHE_PATH, dirname(@__FILE__))
 
 using PreM
 
