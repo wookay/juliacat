ca831d0421cfdcc58943ba5816ff66bd97b09b96
WooKyoung Noh <wookay.noh@gmail.com>
2015-08-13 18:15:56 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,24 @@
 using Base.Test
-@test 1==1
+
+# Hangul
+include("../juliacat/Hangul.jl")
+
+if VERSION.minor > 3 @eval begin
+
+han = hangul_split("한")
+@test Any[["ㅎ","ㅏ","ㄴ"]] == han
+cho = hangul_chosungs("한")
+@test ["ㅎ"] == cho
+@test "한" == hangul_join(["ㅎ","ㅏ","ㄴ"])
+
+end end
+
+
+# ArrayExt
+include("../juliacat/ArrayExt.jl")
+
+@test 2 == index([1,5,3], 5)
+
+
+# UnitTest
+include("../juliacat/UnitTest.jl")
