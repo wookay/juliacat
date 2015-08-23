40513ae658043ba259ff6740a03358f63aedcdf0
WooKyoung Noh <wookay.noh@gmail.com>
2015-08-23 11:31:13 +0200

    vararg

--- TestCases/test_core.jl
+++ TestCases/test_core.jl
@@ -17,14 +17,13 @@ function test_equality()
   assert_true(-0 === 0)
   assert_false(-0.0 === 0.0)
 
-if VERSION.minor > 3 @eval begin
-  assert_equal(Core, @which is)
-
-  assert_true(1 ≈ 1.00000001)
-  assert_false(1 ≈ 1.0000001)
-
-end end
-
+  if VERSION.minor > 3 @eval begin
+    assert_equal(Core, @which is)
+  
+    assert_true(1 ≈ 1.00000001)
+    assert_false(1 ≈ 1.0000001)
+  
+  end end
 end
 
 function test_char()
@@ -32,21 +31,38 @@ function test_char()
   assert_true(isa(x, Char))
   assert_isa(x, Char)
 
-if VERSION.minor > 3 @eval begin
-  x = '\U1f355'
-  assert_equal(2, charwidth(x))
-end end
+  if VERSION.minor > 3 @eval begin
+    x = '\U1f355'
+    assert_equal(2, charwidth(x))
+  end end
 
 end
+
 
 function test_nothing()
   assert_equal(nothing, eval(symbol(string(nothing))))
 
-if VERSION.minor > 3 @eval begin
-  assert_isa(nothing, Void)
-end end
+  if VERSION.minor > 3 @eval begin
+    assert_isa(nothing, Void)
+  end end
 
 end
+
+
+
+if VERSION.minor > 3 @eval begin
+
+function test_vararg()
+  +(a::Tuple{Vararg{Int}}) = string(a)
+  assert_equal("()"      , +(())      )
+  assert_equal("(1,)"    , +((1...))  )
+  assert_equal("(1,)"    , +((1,))    )
+  assert_equal("(1,2)"   , +((1,2))   )
+  assert_equal("(1,2,3)" , +((1,2,3)) )
+  Base.Test.@test_throws MethodError +((1))
+end
+
+end end
 
 
 function test_invoke()
