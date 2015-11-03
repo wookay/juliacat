fd1e59f7438ec8ccd1f47680f2cb70ff3ddd0e9a
wookyoung <wookay.noh@gmail.com>
2015-11-03 16:22:08 +0900

    test reactive

--- TestCases/test_reactive.jl
+++ TestCases/test_reactive.jl
@@ -14,8 +14,20 @@ end
 @loading Reactive
 
 function test_reactive()
-  a = Input(10)
-  assert_isa(a, Reactive.Input{Int})
+  n = Input(100)
+  assert_isa(n, Reactive.Input{Int})
+  assert_equal(100, value(n))
+	push!(n, 5)
+  assert_equal(5, value(n))
+
+	double = lift(x->2x, n)
+  assert_isa(double, Reactive.Lift{Int})
+  assert_equal(5, value(n))
+  assert_equal(10, value(double))
+
+	push!(n, 15)
+  assert_equal(15, value(n))
+  assert_equal(30, value(double))
 end
 
 if is_main()
